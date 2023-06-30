a69370187b76061db3154aaf397b70afc8313e81
ThMenezes <thita.menezes24@gmail.com>
2023-06-29 22:28:58 -0300

    Adicionando função ao botão de quantidade

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,13 @@
 let valorTotal = [0,0];
-let valorProdto = [50.00,30.00];
+let valorProduto = [50.00,30.00];
 let qtd = [0,0];
 
+function adicionarItem(item){
+    let quantidade = document.getElementById('quantidade' + item);
+    let total = document.getElementById("total" + item);
+    qtd[item] += 1;
+    valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+    quantidade.innerHTML = qtd[item];
+
+    total.innerHTML = valorTotal[item].toFixed(2);
+}
