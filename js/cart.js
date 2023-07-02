931935f8c9aeaa4c28f93801bd559bcc003e37ef
ThMenezes <thita.menezes24@gmail.com>
2023-07-02 11:04:57 -0300

    adicionando funcão ao botão de subtração

--- js/cart.js
+++ js/cart.js
@@ -11,3 +11,14 @@ function adicionarItem(item){
 
     total.innerHTML = valorTotal[item].toFixed(2);
 }
+
+function removerItem(item) {
+    if(qtd[item] > 0) {
+        qtd[item] -=1;
+        let quantidade = document.getElementById("quantidade" + item);
+        let total = document.getElementById('total' + item);
+        quantidade.innerHTML = qtd[item];
+        valorTotal[item] = Number.parseFloat(valorProduto[item] * qtd[item]);
+        total.innerHTML = valorTotal[item].toFixed(2);
+    }
+}
